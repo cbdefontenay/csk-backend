808ac661b4bcc0b6ada505189c1cf7bcab90e49d
Cyprien <cbdefontenay@gmail.com>
2024-11-28 23:27:22 +0100

    ajout des traductions tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
-use axum::{routing::get, Router};
+use fs::read_to_string;
+use axum::body::Body;
+use axum::{
+    extract::Path,
+    http::{HeaderMap, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use std::fs;
 use tower_http::cors::{Any, CorsLayer};
 
 async fn hello_world() -> &'static str {
     "Hello, world!!"
+}
+
+async fn get_translation(Path(lang): Path<String>, _headers: HeaderMap) -> impl IntoResponse {
+    let lang = if lang.is_empty() {
+       String::from("en")
+    } else {
+        lang
+    };
+
+    let path = format!("./src/translations/{}.json", lang);
+
+    match read_to_string(path) {
+        Ok(content) => (StatusCode::OK, content).into_response(),
+        Err(_) => (StatusCode::NOT_FOUND, Body::from("Translation not found")).into_response(),
+    }
 }
 
 #[shuttle_runtime::main]
@@ -13,7 +37,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .allow_headers(Any);
 
     let router = Router::new()
-        .route("/hello-world", get(hello_world))
+        .route("/", get(hello_world))
+        .route("/translations/:lang", get(get_translation))
         .layer(cors);
 
     Ok(router.into())
